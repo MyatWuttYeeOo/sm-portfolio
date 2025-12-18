8ec79cced886231c8a8444b182e9134653bd2458
Myat Wutt Yee Oo <wuttyee582@gmail.com>
2025-12-18 23:36:41 +0630

    refactor-commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const mobileNavToggleBtn = document.querySelector(".mobile-nav-toggle");
+const mobileNavLinks = document.querySelector(".mobile-nav-links");
+const mobileNavToggleLine2 = document.querySelector(
+    ".mobile-nav-toggle-line-2"
+);
+const mobileNavToggleLine3 = document.querySelector(
+    ".mobile-nav-toggle-line-3"
+);
+const mobileNavToggleLine1 = document.querySelector(
+    ".mobile-nav-toggle-line-1"
+);
+mobileNavToggleBtn.addEventListener("click", () => {
+    mobileNavLinks.classList.toggle("active-mobile-nav-links");
+    mobileNavToggleLine2.classList.toggle("active-mobile-nav-toggle-line-2");
+    mobileNavToggleLine1.classList.toggle("active-mobile-nav-toggle-line-1");
+    mobileNavToggleLine3.classList.toggle("active-mobile-nav-toggle-line-3");
+
+
+});
+mobileNavLinks.addEventListener("click", (e) => {
+    if((e.target.tagName = "a")){
+    mobileNavLinks.classList.toggle("active-mobile-nav-links")
+    mobileNavToggleLine2.classList.toggle("active-mobile-nav-toggle-line-2");
+    mobileNavToggleLine1.classList.toggle("active-mobile-nav-toggle-line-1");
+    mobileNavToggleLine3.classList.toggle("active-mobile-nav-toggle-line-3");
+
+    }
+}
+)
